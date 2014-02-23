12e90385809182439969742594ba0e2233d7bfc1
TOSHIBA <TOSHIBA@TOSHIBA-PC>
2014-02-23 10:48:54 +0700

    Class A

---
+++ Tugas1/src/main/java/itb/stei/rpl/ppl/tugas1/packagea/A.java
@@ -0,0 +1,17 @@
+/**
+ * @author Asri Maspupah
+ */
+
+
+package itb.stei.rpl.ppl.tugas1.packagea;
+
+public class A {
+	public A() {
+		// TODO Auto-generated constructor stub
+		System.out.println("ctor-A");
+	}
+	
+	public void f(){
+		System.out.println("A");
+	}
+}
