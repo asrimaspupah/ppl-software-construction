e674067d4a22a5504cc5f382f2b9680e0a62822e
edbert <edbert@edbert-pc>
2014-02-24 09:24:02 +0700

    new commit

---
+++ Tugas1/src/main/java/itb/stei/rpl/ppl/tugas1/AllConst.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package itb.stei.rpl.ppl.tugas1;
+
+/**
+ *
+ * @author edbert
+ */
+public final class AllConst {
+    public static final double PI = 3.1417;
+    public static final double RHO = 9.0;
+    public static final double EPSILON = 0.005;
+    public static final int N = 8;
+}
--- Tugas1/src/main/java/itb/stei/rpl/ppl/tugas1/MainProg.java
+++ Tugas1/src/main/java/itb/stei/rpl/ppl/tugas1/MainProg.java
@@ -4,7 +4,11 @@ package itb.stei.rpl.ppl.tugas1;
  *
  * @author edbert
  */
-public class Main {
+public class MainProg implements X {
+    
+    public int w() {
+        return 2;
+    }
     
     public static void main(String[] args) {
         
