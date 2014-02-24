394331cbf794096063f8ffb13fff1805650d00ef
edbert <edbert@edbert-pc>
2014-02-24 15:14:03 +0700

    new commit

---
+++ Tugas1/src/test/java/itb/stei/rpl/ppl/tugas1/MainProgTest.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package itb.stei.rpl.ppl.tugas1;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author edbert
+ */
+public class MainProgTest {
+    
+    public MainProgTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void wTest() {
+        MainProg mainProg = new MainProg();
+        assertEquals(2, mainProg.w());
+    }
+}
