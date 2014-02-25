a61d27a8b910a2d5819942f04997f519876342e6
asri maspupah <asri.maspupah@yahoo.com>
2014-02-25 12:26:58 +0700

    Penambahan Coment di Kelas A, AB dan ABB

--- Tugas1/src/main/java/itb/stei/rpl/ppl/tugas1/packagea/A.java
+++ Tugas1/src/main/java/itb/stei/rpl/ppl/tugas1/packagea/A.java
@@ -1,17 +1,26 @@
 /**
  * @author Asri Maspupah
+ * Kelas A 
  */
 
 
 package itb.stei.rpl.ppl.tugas1.packagea;
 
 public class A {
-	public A() {
-		// TODO Auto-generated constructor stub
-		System.out.println("ctor-A");
-	}
 	
-	public void f(){
-		System.out.println("A");
-	}
+    /**
+     * Kontruktor dari Kelas A membuat objek Kelas A dengan
+     * Melakukan print "ctor-A" ke layar
+     */
+    public A() {
+	// TODO Auto-generated constructor stub
+	System.out.println("ctor-A");
+    }
+	
+    /**
+     * Sebuah method untuk melakukan print "A" ke layar
+     */
+    public void f(){
+	System.out.println("A");
+    }
 }
--- Tugas1/src/main/java/itb/stei/rpl/ppl/tugas1/packagea/ABB.java
+++ Tugas1/src/main/java/itb/stei/rpl/ppl/tugas1/packagea/ABB.java
@@ -1,29 +1,45 @@
 /**
  * @author Asri Maspupah
+ * Kelas ABB adalah turunan dari kelas AB
  */
 
 package itb.stei.rpl.ppl.tugas1.packagea;
 
 public class ABB extends AB {
-	private int x;
+    private int x;
 	
-	public ABB() {
-		System.out.println("ctor-ABB");
-		x = 0;
-	}
+     /**
+     * Kontruktor dari Kelas ABB membuat objek Kelas A dengan
+     * melakukan print "ctor-ABB" ke layar dan mengisi atribut x = 0
+     */
+    public ABB() {
+	System.out.println("ctor-ABB");
+	x = 0;
+    }
 
-	public ABB(int newx) {
-		x = newx;
-	}
-	
-	@Override
-	public void f() {
-		System.out.println("ABB");
-	}
-	
-	@Override
-	public String toString() {
-		return ("nilai atribut x = " + x);
-	}
-	
+    /**
+    * Kontruktor dari Kelas ABB membuat objek Kelas A dengan
+    * mengisi atribut x = parameter input
+    * @param integer newx
+    */
+    public ABB(int newx) {
+	x = newx;
+    }
+
+    /**
+    * Sebuah method yang meng-override method f() dari parent (kelas AB)
+    * dengan melakukan print "ABB" ke layar
+    */
+    @Override
+    public void f() {
+	System.out.println("ABB");
+    }
+
+    /**
+    * Sebuah method untuk menampilkan nilai x ke layar
+    */
+    @Override
+    public String toString() {
+	return ("nilai atribut x = " + x);
+    }	
 }
