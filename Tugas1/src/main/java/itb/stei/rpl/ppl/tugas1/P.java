f5773f1badb7cfe4c760cffa7093958d307cc709
ade sukendar <23513088@std.stei.itb.ac.id>
2014-02-25 11:47:47 +0700

    class P dan Q, Test dan komentar ok

--- Tugas1/src/main/java/itb/stei/rpl/ppl/tugas1/P.java
+++ Tugas1/src/main/java/itb/stei/rpl/ppl/tugas1/P.java
@@ -7,9 +7,16 @@ package itb.stei.rpl.ppl.tugas1;
 /**
  *
  * @author adesuk
+ * 
+ * Kelas berisi konstruktor yang akan mencetak 
+ * sebuah tipe string ke layar
  */
 public class P {
 
+    /**
+     * Kostruktor default
+     * Mencetak "ctor-P" ke layar
+     */
     public P() {
         System.out.println("ctor-P");
     }
--- Tugas1/src/main/java/itb/stei/rpl/ppl/tugas1/Q.java
+++ Tugas1/src/main/java/itb/stei/rpl/ppl/tugas1/Q.java
@@ -7,9 +7,17 @@ package itb.stei.rpl.ppl.tugas1;
 /**
  *
  * @author adesuk
+ * 
+ * Kelas berisi konstruktor yang akan mencetak 
+ * sebuah tipe string ke layar
+ *
  */
 public class Q {
 
+    /**
+     * Kostruktor default
+     * Mencetak "ctor-Q" ke layar
+     */
     public Q() {
         System.out.println("ctor-Q");
     }
