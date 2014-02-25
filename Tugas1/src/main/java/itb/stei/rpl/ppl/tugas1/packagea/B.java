301db837a270cd903a3fb19a571c91a8322c631b
wirasta1330 <wendi@kuyainside.com>
2014-02-25 13:03:05 +0700

    membuat interface B dan C

---
+++ Tugas1/src/main/java/itb/stei/rpl/ppl/tugas1/packagea/B.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package itb.stei.rpl.ppl.tugas1.packagea;
+
+/**
+ *
+ * @author wirasta1330
+ */
+public interface B {
+    void v(); 
+}
